c64a64d9c638daba8f8387b67413b91cda037296
victor <vicrosa25@gmail.com>
2016-07-20 12:16:53 +0200

    Initial Commit

---
+++ Jakefile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by victor on 19/07/16.
+ */
+(function () {
+    "use strict";
+
+    desc('Default Build');
+    task("default", function () {
+        console.log("\n\nBUILD OK");
+    });
+
+}());
---
+++ readme.md
@@ -0,0 +1,7 @@
+How to: Tabs
+============
+
+To try the code on your computer
+--------------------------------
+
+To run the build type 'node build.js'.
