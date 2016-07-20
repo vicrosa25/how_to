0afdcb2f294d612d329d76b25687a691e11271be
victor <vicrosa25@gmail.com>
2016-07-20 14:06:12 +0200

    Build script Node version

--- Jakefile.js
+++ Jakefile.js
@@ -4,9 +4,24 @@
 (function () {
     "use strict";
 
+    var EXPECTED_NODE_VERSION = "v4.2.6";
+
+
     desc('Default Build');
-    task("default", function () {
+    task("default", [ "version"],function () {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check the node version");
+    task("version", function () {
+        console.log("Cheking the node version: .");
+
+        var actualVersion = process.version;
+
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Incorrect node version: Expected " + EXPECTED_NODE_VERSION + " but was " + actualVersion);
+        }
+
     });
 
 }());
