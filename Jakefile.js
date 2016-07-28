51ce8cbf8a24a043abf060538741caadccb5a687
victor <vicrosa25@gmail.com>
2016-07-28 13:41:25 +0200

    Cleaned up lint task in Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -48,29 +48,37 @@
 
         jshint.checkFiles({
             files: [ "Jakefile.js", "src/**/*.js" ],
-            options: {
-                bitwise: true,
-                eqeqeq: true,
-                forin: true,
-                freeze: true,
-                futurehostile: true,
-                latedef: "nofunc",
-                nonew: true,
-                strict: true,
-                undef: true,
-                node: true,
-                browser: true,
-            },
-            globals: {
-                // Mocha
-                describe: false,
-                it: false,
-                before: false,
-                after: false,
-                beforeEach: false,
-                afterEach: false
-            }
+            options: lintOptions(),
+            globals: lintGlobals()
         }, complete, fail);
     }, {async: true});
+
+    function lintOptions() {
+        return {
+            bitwise: true,
+            eqeqeq: true,
+            forin: true,
+            freeze: true,
+            futurehostile: true,
+            latedef: "nofunc",
+            nonew: true,
+            strict: true,
+            undef: true,
+            node: true,
+            browser: true
+        };
+    }
+
+    function lintGlobals() {
+        return {
+            // Mocha
+            describe: false,
+                it: false,
+            before: false,
+            after: false,
+            beforeEach: false,
+            afterEach: false
+        };
+    }
 
 }());
