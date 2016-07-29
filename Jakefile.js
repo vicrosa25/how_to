065f8fb8714eee682be901208c4e5abf62e42989
victor <vicrosa25@gmail.com>
2016-07-29 19:56:36 +0200

    Stubbed in 'clean' and 'build' tasks

--- Jakefile.js
+++ Jakefile.js
@@ -8,6 +8,7 @@
 
     var jshint = require("simplebuild-jshint");
     var karma = require("simplebuild-karma");
+    var DIST_DIR = "generated/dist";
 
 
     var KARMA_CONFIG = "karma.conf.js";
@@ -28,13 +29,18 @@
     });
 
     desc("Run a localhost");
-    task("run", function () {
+    task("run", ["build"], function () {
 
-        jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true}, complete);
+        jake.exec("node node_modules/http-server/bin/http-server" + DIST_DIR, {interactive: true}, complete);
 
 
         console.log("Run http-server here");
     }, {async: true});
+
+    desc("Erase all generated files");
+    task("clean", function () {
+       console.log("Erasing generated files: .");
+    });
 
 
     //**** Supporting tasks
@@ -77,6 +83,13 @@
             strict: !process.env.loose
         }, complete,fail);
     }, {async: true});
+
+    desc("Build distribution directory");
+    task("build", [DIST_DIR], function () {
+       console.log("Building distribution directory:");
+    });
+
+    directory(DIST_DIR);
 
     function lintOptions() {
         return {
