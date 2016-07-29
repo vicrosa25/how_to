6303e8dadbf15eaac18f307acb384e83dcdc55c9
victor <vicrosa25@gmail.com>
2016-07-29 19:39:04 +0200

    Ignore generated files

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    var addition = requires("./addition.js");
+    var addition = require("./addition.js");
 
     console.log("Hello!");
     console.log("42 + 13: " + addition.add(42, 13));
