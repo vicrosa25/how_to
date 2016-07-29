aa8155e2c4dc24b8e9bfd3b3f674a308bfdbd519
victor <vicrosa25@gmail.com>
2016-07-29 18:55:10 +0200

    Created simple demostration app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by victor on 29/07/16.
+ */
+
+(function () {
+    "use strict";
+
+    var addition = requires("./addition.js");
+
+    console.log("Hello!");
+    console.log("42 + 13: " + addition.add(42, 13)); 
+
+}());
