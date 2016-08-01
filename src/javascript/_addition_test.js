7d8ca6148a02b537394abe54ff7df40930a4ad60
victor <vicrosa25@gmail.com>
2016-08-01 13:10:40 +0200

    Isolated chai behing assert.js wrapper

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -5,7 +5,7 @@
     "use strict";
 
     var addition = require("./addition.js");
-    var assert = require("../vendor/chai-3.5.0.js").assert;
+    var assert = require("./assert.js");
 
     describe("Addition", function () {
 
---
+++ src/javascript/assert.js
@@ -0,0 +1,11 @@
+/**
+ * Created by victor on 1/08/16.
+ */
+(function () {
+    "use strict";
+
+    var assert = require("../vendor/chai-3.5.0.js").assert;
+
+    module.exports = assert;
+
+}());
