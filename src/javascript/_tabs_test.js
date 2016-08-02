2cb1166be91005043ee9e7703c2c40a867c99415
victor <vicrosa25@gmail.com>
2016-08-02 12:27:47 +0200

    Created tabs library (doesn't do anything yet)

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -3,11 +3,14 @@
  */
 (function () {
     "use strict";
-    
-    var assert = require("./assert.js");
 
-    describe("Something", function () {
-        it("Something", function () {
+    var assert = require("./assert.js");
+    var tabs = require("./tabs.js");
+
+    describe("Tabs", function () {
+        it("has an API", function () {
+
+            tabs.initialize();
 
         });
     });
---
+++ src/javascript/tabs.js
@@ -0,0 +1,11 @@
+/**
+ * Created by victor on 2/08/16.
+ */
+(function () {
+    "use strict";
+
+    exports.initialize = function initialize() {
+
+    };
+
+}());
