d232f4974472025475651a02b12a5d99b8e133c2
victor <vicrosa25@gmail.com>
2016-08-03 12:09:53 +0200

    Modified tabs library to use classlist rather than setting class attribute

--- src/javascript/_tabs_test.js
+++ src/javascript/_tabs_test.js
@@ -8,23 +8,20 @@
     var tabs = require("./tabs.js");
 
     describe("Tabs", function () {
-        it("sets a class on an element", function () {
+        it("sets a class on an element when element has no existing classes", function () {
             var element = addElement("div");
             tabs.initialize(element, "someClass");
             assert.equal(getClass(element), "someClass");
             removeElement(element);
         });
 
-        it("sets a class on a element withoute eraising existing class"), function () {
+        it("sets a class on a element withoute eraising existing class", function () {
             var element = addElement("div");
             element.setAttribute("class", "existingClass");
-
             tabs.initialize(element, "newClass");
-
-            assert.equal(getClass(element, "existingClass someClass"));
-
+            assert.equal(getClass(element), "existingClass newClass");
             removeElement(element);
-        }
+        });
 
         function getClass(element) {
             return element.getAttribute("class");
